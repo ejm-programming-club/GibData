7f0a0fa8a07ebc6021cc08db6cf22a45a0bf5abd
trakjohnson <trakjohnsonweb@gmail.com>
2018-11-01 15:11:35 +0100

    Allow populating of Block 3 and Block 5

--- src/data_organizer.py
+++ src/data_organizer.py
@@ -1,11 +1,19 @@
 import csv
 import sqlite3
-import os.path
+import os
 
 
 DATA_FILE_NAME = "c2018.csv"
 DB_FILE_NAME = "c2018.db"
-DATA_FOLDER = "../data"
+DATA_FOLDER = "data"
+
+CATEGORIES = {
+    "PAPER ONE": "paper1", "PAPER TWO": "paper2", "PAPER THREE": "paper3",
+    "PROJECT": "project", "EXPLORATION": "exploration", "INTERNAL ASSESSMENT": "ia"
+}
+SCIENCES = ["BIOLOGY", "CHEMISTRY", "PHYSICS"]
+LANGUAGES = ["ENGLISH", "FRENCH", "SPANISH", "GERMAN", "ITALIAN", "MANDARIN"]
+BLOCK_3 = ["GEOGRAPHY", "HISTORY", "ECONOMICS"]
 
 
 def to_key_value(l):
@@ -23,6 +31,8 @@ def converted_to_db(force_update=False):
         print("Database already created")
         return
 
+    print(os.getcwd())
+
     # Read CSV
     with open(f"{DATA_FOLDER}/{DATA_FILE_NAME}") as f:
         r = csv.reader(f.readlines(), delimiter=";")
@@ -35,15 +45,25 @@ def converted_to_db(force_update=False):
         ("ee_subject", "string"),
         ("ee_grade", "integer"),
         ("tok_presentation_grade", "integer"),
-        ("tok_essay_grade", "integer")
+        ("tok_essay_grade", "integer"),
     ]
 
     for i in range(1, 7):
         # iterate over the 6 blocks
-        if i == 1:
-            # languages
-            table_headers +=[("block1_subject", "string")] + \
-                            [(f"block1_paper{i}_score", "integer") for i in range(1, 4)]
+        # if i == 1 or i == 6:
+        #     # languages (max)
+        #     # TODO check if there are 3 papers in some other places
+        #     n_papers = 3
+        # else:
+        n_papers = 3
+
+        table_headers += [(f"block{i}_subject", "string"), (f"block{i}_level", "string")] + \
+                         [(f"block{i}_paper{p}_score", "integer") for p in range(1, n_papers + 1)]
+
+        if i == 3:
+            table_headers += [("block3_ia_score", "integer")]
+        if i == 5:
+            table_headers += [("block5_project_score", "integer"), ("block5_exploration_score", "integer")]
 
     if force_update:
         cursor.execute(f"DROP TABLE if exists students")
@@ -68,7 +88,7 @@ def converted_to_db(force_update=False):
         subject = row[2]
         level = row[3]
         test_type = row[4]
-        lang = row[5]
+        # lang = row[5]
         # we skip school num (6)
         student_id = row[7]
         component_grade = row[8]
@@ -80,11 +100,28 @@ def converted_to_db(force_update=False):
             ids_already_added.add(student_id)
 
         if test_type == "EXTENDED ESSAY":
+            # EE
             to_set = [("ee_subject", subject), ("ee_grade", scaled_grade)]
         elif level == "TK":
+            # TOK Essay & Presentation
             to_set = [(
                 "tok_presentation_grade" if "PRESENTATION" in test_type else "tok_essay_grade", scaled_grade
             )]
+        elif subject in BLOCK_3:
+            # H/G/ECO (block 3)
+            to_set = [
+                ("block3_subject", subject), ("block3_level", level),
+                (f"block3_{CATEGORIES[test_type]}_score", scaled_grade)
+            ]
+        elif "MATH" in subject:
+            # Maths (block 5)
+            level = level if "STUDIES" not in subject else "MS"
+            to_set = [
+                ("block5_subject", "MATH"), ("block5_level", level),
+                (f"block5_{CATEGORIES[test_type]}_score", scaled_grade)
+            ]
+        else:
+            print(subject, level, test_type)
         cursor.execute(f"UPDATE students SET {to_key_value(to_set)} WHERE id = {student_id}")
 
     conn.commit()
